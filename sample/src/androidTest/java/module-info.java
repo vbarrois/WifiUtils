e15a81a3cbc766acc3d2a10bbca9eed7595f2a3b
Vincent Barrois <vbarrois@gmail.com>
2020-09-22 13:24:46 +0200

    <wifi> first commit

---
+++ sample/src/androidTest/java/module-info.java
@@ -0,0 +1,13 @@
+module wifiUtils {
+	exports com.thanosfisherman.wifiutils.wifiDisconnect;
+	exports com.thanosfisherman.wifiutils.sample;
+	exports com.thanosfisherman.wifiutils.wifiWps;
+	exports com.thanosfisherman.wifiutils.wifiConnect;
+	exports com.thanosfisherman.wifiutils.utils;
+	exports com.thanosfisherman.wifiutils.wifiScan;
+	exports com.thanosfisherman.wifiutils.wifiRemove;
+	exports com.thanosfisherman.wifiutils.wifiState;
+	exports com.thanosfisherman.wifiutils;
+
+	requires WifiUtils;
+}
